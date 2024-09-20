781b429546b6044d66d6a16415e024373a0d6371
nascimentofernanda <nascimentofernandadev@gmail.com>
2024-09-20 14:21:42 -0300

    Resolução dos exercícios do desafio

--- Arquivos @nascimentofernanda/Verificando_números_pares_e_ímpares.py
+++ Arquivos @nascimentofernanda/Verificando_números_pares_e_ímpares.py
@@ -1,3 +1,9 @@
 #Descrição: Como entrada, receba um número inteiro e verifique se ele é par ou ímpar.
 # Uma dica é: Utilize condicionais para realizar a verificação e, se possível, faça uso do Github Copilot(ou outra IA) para otimizar a estrutura do código.
 
+número=int(input("Digite um número inteiro: "))
+
+if(número%2==0):
+    print("O número é par")
+else:
+    print("O número é ímpar")
