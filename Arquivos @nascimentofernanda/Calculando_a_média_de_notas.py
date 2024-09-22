2bdfd66dbd58a2df0ec21c27c48cbf4bbd0dd415
nascimentofernanda <nascimentofernandadev@gmail.com>
2024-09-22 14:25:44 -0300

    Resolução dos exercícios do desafio

---
+++ Arquivos @nascimentofernanda/Calculando_a_média_de_notas.py
@@ -0,0 +1,10 @@
+#Descrição: Agora vamos calcular a média de três notas fornecidas na entrada do usuário. 
+#Uma dica é: Utilize operadores aritméticos para realizar o cálculo da média.
+
+nota1=int(input("Digite a primeira nota: "))
+nota2=int(input("Digite a segunda nota: "))
+nota3=int(input("Digite a terceira nota: "))
+
+média=((nota1+nota2+nota3)/3)
+
+print(f"A média é: {média}.")
